e58f55cf74bca4c6e2afd9158ac08532bda1f4df
Chinmay-Dorge <lcs2020022@iiitl.ac.in>
2022-04-21 10:21:00 +0530

    Added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Home from "./components/Home";
 import NavbarComp from "./components/NavbarComp";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import Dashboard from "./components/Dashboard";
+import SigninPage from './components/SigninPage'
+
+
 function App() {
   return (
     <div className="App">
       <NavbarComp />
-      <Home />
+      <Routes>
+        <Route path='/' element = {<Home />} exact/>
+        <Route path='/dashboard' element={<Dashboard/>} exact/>
+        <Route path='/login' element = {<LoginPage/>} exact/>
+        <Route path='/signup' element = {<SigninPage/>} exact/>
+      </Routes>
+
+      
+      {/* <Home /> */}
     </div>
   );
 }
--- frontend/src/components/NavbarComp.jsx
+++ frontend/src/components/NavbarComp.jsx
@@ -1,22 +1,28 @@
 import React from "react";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import Home from "./Home";
 
 function NavbarComp() {
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">DormEz</Navbar.Brand>
+          <Navbar.Brand href="/">DormEz</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#dashboard">Dashboard</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/dashboard">Dashboard</Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link href="#deets">Login</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
+              <Nav.Link href="/login">Login</Nav.Link>
+              <Nav.Link eventKey={2} href="/signup">
                 SignUp
               </Nav.Link>
             </Nav>
